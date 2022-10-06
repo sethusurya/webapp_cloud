317e2e97ec5318e968dfb1e4992c4b1ec95d78a6
Sethusuryateja Pagolu <pagolu.s@northeastern.edu>
2022-10-06 13:18:01 -0400

    check CI

---
+++ tests/app.test.js
@@ -0,0 +1,15 @@
+const supertest = require('supertest');
+const express = require('express');
+const app = express();
+
+const api = supertest(app)
+
+app.get("/healthz", (req, res) => {
+    res.sendStatus(200);
+  });
+
+test('HTTP Status code is 200, OK', async () => { 
+    await api
+    .get('/healthz')
+    .expect(200)
+})
